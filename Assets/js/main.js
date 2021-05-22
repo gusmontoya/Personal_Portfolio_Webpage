f6c2b23d43748d9cd5e3d4a86a3b874ac99ebe8f
gusmontoya <gus.montoya@gmail.com>
2021-05-22 16:59:26 -0700

    Added a ton

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -95,3 +95,13 @@ const mySwiper = new Swiper('.testimonial_container', {
         }
     }
 });
+
+/*===GSAP ANIMATION===*/
+
+gsap.from('.home_img', {opacity: 0, duration: 2, delay:.5, x:60})
+gsap.from('.home_data', {opacity: 0, duration: 2, delay:.8, y:25})
+gsap.from('.home_greeting, .home_name, .home_profession, .home_button', {opacity: 0, duration: 2, delay:1, y:25, ease:'export.out', stagger:.2})
+
+gsap.from('.nav_logo, .nav_toggle', {opacity: 0, duration: 2, delay:1.5, y:25, ease:'export.out', stagger:.2})
+gsap.from('.nav_item', {opacity: 0, duration: 2, delay:1.8, y:25, ease:'export.out', stagger:.2})
+gsap.from('.home_social-icon', {opacity: 0, duration: 2, delay:2.3, y:25, ease:'export.out', stagger:.2})
